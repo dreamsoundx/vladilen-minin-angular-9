4c5a86f1c5ec7022d05591f6912c5c9e6cb6b642
DreamsoundX <itsdreamsound@gmail.com>
2022-08-16 17:36:59 +0300

    folders are fixed

---
+++ ex16_bonus-typescript-for-angular/task06-generic-types/index.js
@@ -0,0 +1,9 @@
+// const arr: Array<number> = [1, 2, 3, 4]
+var users = [
+    { id: 1, name: 'V', age: 2 },
+    { id: 2, name: 'E', age: 23 },
+];
+var users2 = [
+    { id: 1, name: 'V', age: 2 },
+    { id: 2, name: 'E', age: 23 },
+];
---
+++ ex16_bonus-typescript-for-angular/task06-generic-types/index.ts
@@ -0,0 +1,17 @@
+// const arr: Array<number> = [1, 2, 3, 4]
+
+interface User {
+  id: number;
+  name: string;
+  age: number;
+}
+
+const users: Array<User> = [
+  { id: 1, name: 'V', age: 2 },
+  { id: 2, name: 'E', age: 23 },
+];
+
+const users2: User[] = [
+  { id: 1, name: 'V', age: 2 },
+  { id: 2, name: 'E', age: 23 },
+];
